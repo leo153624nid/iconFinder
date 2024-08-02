8b0f5b69ab2a840fc293dbbed09e42f58a0e3ade
Aleksey Chaykin <barjastroy@gmail.com>
2024-08-02 10:06:08 +0300

    Initial Commit

---
+++ iconFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iconFinder
+//
+//  Created by A Ch on 02.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
